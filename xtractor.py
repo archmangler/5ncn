73fb52183937233d902cb174984c17c599555657
Traiano Welcome <traiano@gmail.com>
2018-12-02 23:58:34 +0800

    elabourate our script to parse outputs

--- xtractor.py
+++ xtractor.py
@@ -7,6 +7,10 @@ import os
 debug = 0
 inventory=[]
 
+def exec_some_random_command(item):
+ cmd = "tower-cli add this to an ansible inventory "+item
+ return cmd
+
 stuff = subprocess.check_output(['terraform', 'output', '-json','cluster_slave_private_addresses'])
 if debug ==1:
  print stuff 
@@ -16,4 +20,5 @@ if debug==1:
 inventory=list(j['value'])
 
 for i in inventory:
- print i
+ cmd = exec_some_random_command(i)
+ print cmd
